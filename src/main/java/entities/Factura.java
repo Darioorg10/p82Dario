ffd8598905a451ec64e46d2603ae490eb9b86963
darioorg10 <dricgar1103@g.educaand.es>
2023-05-11 12:45:29 +0200

    Entidades, controladores y excepciones creadas, también la ventana principal de la aplicación

---
+++ src/main/java/entities/Factura.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author dario
+ */
+@Entity
+@Table(name = "factura")
+@NamedQueries({
+    @NamedQuery(name = "Factura.findAll", query = "SELECT f FROM Factura f"),
+    @NamedQuery(name = "Factura.findByCodFactura", query = "SELECT f FROM Factura f WHERE f.codFactura = :codFactura"),
+    @NamedQuery(name = "Factura.findByFechaEmision", query = "SELECT f FROM Factura f WHERE f.fechaEmision = :fechaEmision"),
+    @NamedQuery(name = "Factura.findByDescripcion", query = "SELECT f FROM Factura f WHERE f.descripcion = :descripcion"),
+    @NamedQuery(name = "Factura.findByTotalImporteFactura", query = "SELECT f FROM Factura f WHERE f.totalImporteFactura = :totalImporteFactura")})
+public class Factura implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "codFactura")
+    private Integer codFactura;
+    @Column(name = "fechaEmision")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEmision;
+    @Column(name = "descripcion")
+    private String descripcion;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "totalImporteFactura")
+    private Double totalImporteFactura;
+
+    public Factura() {
+    }
+
+    public Factura(Integer codFactura) {
+        this.codFactura = codFactura;
+    }
+
+    public Integer getCodFactura() {
+        return codFactura;
+    }
+
+    public void setCodFactura(Integer codFactura) {
+        this.codFactura = codFactura;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getTotalImporteFactura() {
+        return totalImporteFactura;
+    }
+
+    public void setTotalImporteFactura(Double totalImporteFactura) {
+        this.totalImporteFactura = totalImporteFactura;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codFactura != null ? codFactura.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Factura)) {
+            return false;
+        }
+        Factura other = (Factura) object;
+        if ((this.codFactura == null && other.codFactura != null) || (this.codFactura != null && !this.codFactura.equals(other.codFactura))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.p82dario.Factura[ codFactura=" + codFactura + " ]";
+    }
+    
+}
